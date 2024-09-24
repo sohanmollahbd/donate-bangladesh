51ab60cd21213bc3af22c2036b7888e1b275518a
sohanmollahbd <sohanmollahbd@gmail.com>
2024-09-24 11:03:26 +0600

    Added Feni donation transaction method

--- js/addMoney.js
+++ js/addMoney.js
@@ -25,7 +25,7 @@ document.getElementById('btn-donate-noakhali').addEventListener('click',function
       p.style.border = '1px solid black';
       p.style.padding = '40px';
       p.style.borderRadius = '10px'
-      p.innerText = ` ${addMoney} Taka is Donated for famine-2024 at Feni, Bangladesh`;
+      p.innerText = ` ${addMoney} Taka is Donated for famine-2024 at Noakhali, Bangladesh`;
 
       document.getElementById('transactions-container').appendChild(p);
 
@@ -36,7 +36,46 @@ document.getElementById('btn-donate-noakhali').addEventListener('click',function
     }
   }
 
+})
 
 
+
+// Feni donation 
+
+document.getElementById('btn-donate-feni').addEventListener('click',function(event){
+  event.preventDefault();
+
+  const addMoney = getInputFieldValueById('input-add-money-feni');
+  const balance = getTextFieldValueById('account-balance');
+ if(isNaN(addMoney)){
+   alert('failed to add money');
+   return;
+  }
+ else{
+   if(addMoney > 0 && balance > 0 && addMoney <= balance){
+     
+     const newBalance = balance - addMoney;
+     document.getElementById('account-balance').innerText = newBalance;
+     const accountBalanceFeni = document.getElementById('account-balance-feni').innerText;
+     const accountBalanceFeniNumber = parseFloat(accountBalanceFeni);
+     const newBalanceFeni = accountBalanceFeniNumber + addMoney;
+
+     document.getElementById('account-balance-feni').innerText = newBalanceFeni;
+
+     // add to transaction history
+     const p = document.createElement('p');
+     p.style.border = '1px solid black';
+     p.style.padding = '40px';
+     p.style.borderRadius = '10px'
+     p.innerText = ` ${addMoney} Taka is Donated for famine-2024 at Feni, Bangladesh`;
+
+     document.getElementById('transactions-container').appendChild(p);
+
+    
+   }
+   else{
+     alert('Failed to donate money!')
+   }
+ }
 
 })
