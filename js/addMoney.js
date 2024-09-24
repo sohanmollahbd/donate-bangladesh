f546358d4e2eb76d5320f6c0540d8776c4af782f
sohanmollahbd <sohanmollahbd@gmail.com>
2024-09-24 11:16:12 +0600

    Added Quota Protes 2024  donation transaction method

--- js/addMoney.js
+++ js/addMoney.js
@@ -25,7 +25,7 @@ document.getElementById('btn-donate-noakhali').addEventListener('click',function
       p.style.border = '1px solid black';
       p.style.padding = '40px';
       p.style.borderRadius = '10px'
-      p.innerText = ` ${addMoney} Taka is Donated for famine-2024 at Noakhali, Bangladesh`;
+      p.innerText = ` ${addMoney} Taka is Donated for Flood-2024 at Noakhali, Bangladesh`;
 
       document.getElementById('transactions-container').appendChild(p);
 
@@ -36,7 +36,7 @@ document.getElementById('btn-donate-noakhali').addEventListener('click',function
     }
   }
 
-})
+});
 
 
 
@@ -67,7 +67,49 @@ document.getElementById('btn-donate-feni').addEventListener('click',function(eve
      p.style.border = '1px solid black';
      p.style.padding = '40px';
      p.style.borderRadius = '10px'
-     p.innerText = ` ${addMoney} Taka is Donated for famine-2024 at Feni, Bangladesh`;
+     p.innerText = ` ${addMoney} Taka is Donated for Flood Relief-2024 at Feni, Bangladesh`;
+
+     document.getElementById('transactions-container').appendChild(p);
+
+    
+   }
+   else{
+     alert('Failed to donate money!')
+   }
+ }
+
+});
+
+
+
+// Donate Quota Protest 
+
+document.getElementById('btn-donate-quota').addEventListener('click',function(event){
+  event.preventDefault();
+
+  const addMoney = getInputFieldValueById('input-add-money-quota');
+  const balance = getTextFieldValueById('account-balance');
+ if(isNaN(addMoney)){
+   alert('failed to add money');
+   return;
+  }
+ else{
+   if(addMoney > 0 && balance > 0 && addMoney <= balance){
+     
+     const newBalance = balance - addMoney;
+     document.getElementById('account-balance').innerText = newBalance;
+     const accountBalanceQuota = document.getElementById('account-balance-Quota').innerText;
+     const accountBalanceQuotaNumber = parseFloat(accountBalanceQuota);
+     const newBalanceQuota = accountBalanceQuotaNumber + addMoney;
+
+     document.getElementById('account-balance-Quota').innerText = newBalanceQuota;
+
+     // add to transaction history
+     const p = document.createElement('p');
+     p.style.border = '1px solid black';
+     p.style.padding = '40px';
+     p.style.borderRadius = '10px'
+     p.innerText = `${addMoney} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh`;
 
      document.getElementById('transactions-container').appendChild(p);
 
